10af396e41ef24c3916d41aca80472e6e9d210d1
scriptlver <sloowlaah1@gmail.com>
2025-07-01 20:44:17 -0300

    siii

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import prompt from 'prompt'
 import mainPrompt from "./prompts/prompt-main.js";
+import createQRCODE from './services/qr-code/create.js';
 
 async function main() {
   prompt.get(mainPrompt, async (err, choose) => {
-    if (choose.select == '1') console.log("Você escolheu QRCODE");
+    if (choose.select == '1') await createQRCODE();
     if (choose.select == '2') console.log("Você escolheu PASSWORD");
 
   });
---
+++ src/services/qr-code/create.js
@@ -0,0 +1,9 @@
+import prompt from "prompt";
+import promptQRCode from "../prompts/prompt-qrcode.js";
+
+
+async function createQRCODE(){
+
+}
+
+export default createQRCODE;
