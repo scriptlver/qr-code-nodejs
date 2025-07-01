abb7ecc0650e7325b32237f2a00dfb69587769be
scriptlver <sloowlaah1@gmail.com>
2025-07-01 20:49:38 -0300

    simm

--- src/prompts/prompt-qrcode.js
+++ src/prompts/prompt-qrcode.js
@@ -1,5 +1,20 @@
 import chalk from "chalk";
 
-const promptQRCode = [];
+const promptQRCode = [
+    {
+        name: "link",
+        description: chalk.yellow.bold("Digite o texto ou URL para gerar o QR Code:"),
+        pattern: /^.+$/,
+        message: chalk.red.italic("O texto ou URL não pode estar vazio!"),
+        required: true,
+    },
+    {
+        name:"type",
+        description: chalk.yellow.bold("Escolha o tipo de QR Code (1 - NORMAL, 2 - TERMINAL):"),
+        pattern: /^[1-2]+$/,
+        message: chalk.red.italic("Escolha apenas entre 1 ou 2"),
+        required: true,
+    }
+];
 
 export default promptQRCode;
