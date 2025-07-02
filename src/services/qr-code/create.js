327e47c2e77e0418678beeab7b46bdf5064e96b1
scriptlver <sloowlaah1@gmail.com>
2025-07-01 21:07:28 -0300

    amoooo

--- src/services/qr-code/create.js
+++ src/services/qr-code/create.js
@@ -1,6 +1,6 @@
 import prompt from "prompt";
-import promptQRCode from "../prompts/prompt-qrcode.js";
-
+import promptQRCODE from '../../prompts/prompt-qrcode.js';
+import handle from "./handle.js";
 
 async function createQRCODE(){
     prompt.get(promptQRCode);
